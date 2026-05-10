0605ad77095dcea157de15da0fa7f90d376de6c6
재욱 <haensol976520@gmail.com>
2026-05-10 15:33:50 +0900

    feat: zaemoru 디자인 시스템 통합

---
+++ app/providers.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import "@zaemoru/react";
+
+import type { ReactNode } from "react";
+
+export function Providers({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
